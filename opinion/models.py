a4b8258a4a65a3519fc4dfe3ee3147ac38c5d246
Ryu4949 <kyong494949@gmail.com>
2022-08-12 23:10:01 +0900

    Chore: set Opinion model
    opinion 모델 설정
    settings.py와 urls.py에 이미지 업로드를 위한 설정 추가

--- opinion/models.py
+++ opinion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from better_b_cut import settings
+from movie.models import Movie
 
 # Create your models here.
+class Opinion(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    img = models.ImageField(upload_to='media/', blank=True, null=True)
