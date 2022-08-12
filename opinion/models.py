2895720d4b0f98f7e00d15fb744bdda5ff8dd667
Ryu4949 <kyong494949@gmail.com>
2022-08-12 23:20:47 +0900

    Chore: set model opinion comment, like

--- opinion/models.py
+++ opinion/models.py
@@ -5,9 +5,24 @@ from movie.models import Movie
 # Create your models here.
 class Opinion(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='opinions')
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     img = models.ImageField(upload_to='media/', blank=True, null=True)
+
+class OpinionLike(models.Model):
+    opinion = models.ForeignKey(Opinion, on_delete=models.CASCADE, related_name='opinion_like')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='like_opinion')
+    is_like = models.BooleanField(null=True)
+
+class OpinionComment(models.Model):
+    opinion = models.ForeignKey(Opinion, on_delete=models.CASCADE, related_name='opinion_comment')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OpinionCommentLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    opinion_comment = models.ForeignKey(OpinionComment, on_delete=models.CASCADE, related_name='comment_like')
