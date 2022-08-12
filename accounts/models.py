8d49ea7dd58452c4eb3b1705bd9b2836a8f9f945
이현진 <hjnee1102@gmail.com>
2022-08-12 20:34:41 +0900

    Chore: set model user
    유저 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# password, is_active, last_login
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=8)
+    signup_date = models.DateTimeField(auto_now_add=True)
+    point = models.IntegerField()
+
+    class Meta:
+        db_table = 'user'
+
+    # objects = CustomUserManager()
