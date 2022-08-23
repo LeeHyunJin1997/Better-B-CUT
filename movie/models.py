91275215fb06ebdc442930ba726486fad8d72d39
LeeHyunJin1997 <hjnee1102s@gmail.com>
2022-08-23 15:44:54 +0900

    Chore: set Movie model Movie 모델 생성 poster컬럼 URLfield로 변경

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=16)
+    poster = models.URLField(max_length=200)
