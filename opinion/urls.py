cfd3327838430546a306506a6be7ea362c5ceeb2
LeeHyunJin1997 <hjnee1102s@gmail.com>
2022-08-29 03:03:48 +0900

    Chore: set opinion urls better_b_cut/urls에서 opinion/urls include urls 작성 settings INSTALLED_APPS에 rest_framework 추가 views에 api_view 작성

---
+++ opinion/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'opinion'
+
+urlpatterns = [
+    # opinion
+    path('', views.opinion_list, name='opinion_list'),
+    path('/<int:opinion_id>', views.opinion_detail, name='opinion_detail'),
+    path('/<int:opinion_id>/like', views.like, name='like'),
+    path('/<int:opinion_id>/dislike', views.dislike, name='dislike'),
+    # comment
+    path('/<int:opinion_id>/comment', views.comment_list, name='comment_list'),
+    path('/<int:opinion_id>/comment/<int:opinion_comment_id>', views.comment_detail, name='comment_datail'),
+    path('/<int:opinion_id>/comment/<int:opinion_comment_id>/like', views.comment_like, name='comment_like'),
+]
--- opinion/views.py
+++ opinion/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def opinion_list(request):
+    pass
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def opinion_detail(request):
+    pass
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def like(request):
+    pass
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def dislike(request):
+    pass
+
+
+@api_view(['GET', 'POST'])
+def comment_list(request):
+    pass
+
+
+@api_view(['PUT', 'DELETE'])
+def comment_detail(request):
+    pass
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def comment_like(request):
+    pass
