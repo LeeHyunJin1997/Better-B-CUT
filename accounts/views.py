efda6a99938029459c9a8c436f5b5a00d29cf8d8
Ryu4949 <kyong494949@gmail.com>
2022-09-02 03:16:31 +0900

    chore: set profile views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from .serializers import ProfileSerializer
 # Create your views here.
+
+User = get_user_model()
+
+@api_view(['GET', 'PUT'])
+def profile(request, user_id):
+    user = get_object_or_404(User, id=user_id):
+    serializer = ProfileSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+def follow(request, user_id):
+    pass
