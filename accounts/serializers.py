7fd6afa309c0539133efda470c40ceeec8e38615
Ryu4949 <kyong494949@gmail.com>
2022-09-02 20:27:17 +0900

    Feat: user profile, follow

---
+++ accounts/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from adaptation.serializers import AdaptationListSerializer
+from opinion.serializers import OpinionListSerializer
+from original.serializers import OriginalListSerializer
+from django.contrib.auth import get_user_model
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class FollowListSerializer(serializers.ModelSerializer):
+
+        class Meta: 
+            model = get_user_model()
+            fields = ('pk', 'nickname',)
+    
+    adaptations = AdaptationListSerializer(many=True)
+    opinion_set = OpinionListSerializer(many=True)
+    originals = OriginalListSerializer(many=True)
+    to_user = FollowListSerializer(many=True)
+    from_user = FollowListSerializer(many=True)
+    
+
+    class Meta:
+        model = get_user_model()
+        fields = ('pk', 'nickname', 'point', 'adaptations', 'opinion_set', 'originals', 'to_user', 'from_user',)
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from .serializers import ProfileSerializer
+from .models import Follow
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 User = get_user_model()
@@ -15,5 +17,23 @@ def profile(request, user_id):
     serializer = ProfileSerializer(user)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
+@login_required
+@api_view(['POST'])
 def follow(request, user_id):
-    pass
+    user = request.user
+    follow_user = User.objects.filter(id=user_id)
+    if user.is_authenticated:
+        #이미 팔로우 한 경우
+        if Follow.objects.filter(from_user=user, to_user=follow_user).exists():
+            follow = Follow.objects.filter(from_user=user, to_user=follow_user)
+            follow.delete()
+            data = {
+                'message': '팔로우가 취소되었습니다.'
+            }
+            return Response(data, status=status.HTTP_204_NO_CONTENT)
+        else:
+            Follow.objects.create(from_user=user, to_user=follow_user)
+            data = {
+                'message': '팔로우 되었습니다.'
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
